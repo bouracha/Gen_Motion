Fixed normalisation in that only dimensions 1 and two were being affected

utils/model.py
@@ -156,7 +156,7 @@ class GCN(nn.Module):
             z = mu + torch.mul(torch.exp(gamma), noise)
             y = z
 
-            self.KL = 0.5*torch.sum(torch.exp(gamma) + torch.square(mu) - 1 - gamma, axis=(1,2))
+            self.KL = 0.5*torch.sum(torch.exp(gamma) + torch.pow(mu, 2) - 1 - gamma, axis=(1,2))
 
         for i in range(self.num_stage//2, self.num_stage):
             y = self.gcbs[i](y)