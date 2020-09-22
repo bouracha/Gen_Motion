28ee643c50348310b517dfdf076f4aa6df76b8b6
bouracha <bouracha@tcd.ie>
2020-09-22 11:33:52 -0400

    interpretability changed to use h3.6m. Also added umap code to visualise

--- interpretability.py
+++ interpretability.py
@@ -24,10 +24,8 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--num_trials', type=int, default='10',
-                    help='number of trials of randomly selected hyperparameters')
-parser.add_argument('--variational', type=bool, default=False, help='true if want to include a latent variable')
-parser.add_argument('--epoch', type=int, default=50, help='number of epochs')
+parser.add_argument('--dataset', type=str, default='h3.6m', help='which dataset to use')
+parser.add_argument('--model_path', type=str, default=None, help='path to checkpoint')
 
 opt = parser.parse_args()
 
@@ -37,16 +35,21 @@ is_cuda = torch.cuda.is_available()
 
 
 
-actions = data_utils.define_actions('all', 'h3.6m', out_of_distribution=False)
-
 input_n = 10
 output_n = 25
 dct_n = 35
 sample_rate = 2
 
 cartesian = False
-node_n = 48
 
+if opt.dataset=='h3.6m':
+    node_n = 48
+    n_z = 384
+    actions = data_utils.define_actions('all', 'h3.6m', out_of_distribution=False)
+elif opt.dataset=='cmu_mocap':
+    node_n = 64
+    actions = data_utils.define_actions('all', 'cmu_mocap', out_of_distribution=False)
+    n_z = 512
 
 
 model = nnmodel.GCN(input_feature=dct_n, hidden_feature=256, p_dropout=0.3,
@@ -56,21 +59,26 @@ if is_cuda:
 print(">>> total params: {:.2f}M".format(sum(p.numel() for p in model.parameters()) / 1000000.0))
 #optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr)
 
-
-model_path_len = 'checkpoint/test/ckpt_main_h3.6m_in10_out10_dctn20_dropout_0.0_var_lambda_0.003_nz_8_lr_0.0005_n_layers_6_last.pth.tar'
+if opt.model_path == None:
+    model_path_len = 'checkpoint/test/ckpt_main_cmu_mocap_in50_out25_dctn35_dropout_0.3_var_lambda_0.003_nz_8_lr_0.0005_n_layers_6_last.pth.tar'
+#model_path_len = 'checkpoint/test/ckpt_main_h3.6m_in10_out10_dctn20_dropout_0.0_var_lambda_0.003_nz_8_lr_0.0005_n_layers_6_last.pth.tar'
 print(">>> loading ckpt len from '{}'".format(model_path_len))
 if is_cuda:
     ckpt = torch.load(model_path_len)
 else:
     ckpt = torch.load(model_path_len, map_location='cpu')
 
-batch_size=128
+batch_size=512
 train_data = dict()
 test_data = dict()
 for act in actions:
     print("Loading action {} for train set".format(act))
-    train_dataset = H36motion(path_to_data='h3.6m/', actions=[act], input_n=input_n, output_n=output_n,
-                               sample_rate=sample_rate, split=0, dct_n=dct_n)
+    if opt.dataset == 'h3.6m':
+        train_dataset = H36motion(path_to_data='h3.6m/', actions=[act], input_n=input_n, output_n=output_n,
+                                  sample_rate=sample_rate, split=0, dct_n=dct_n)
+    elif opt.dataset == 'cmu_mocap':
+        train_dataset = CMU_Motion(path_to_data='cmu_mocap/', actions=[act], input_n=input_n, output_n=output_n,
+                                   split=0, dct_n=dct_n)
     #print(train_dataset.__len__())
     data_std = train_dataset.data_std
     data_mean = train_dataset.data_mean
@@ -82,8 +90,12 @@ for act in actions:
         num_workers=10,
         pin_memory=True)
     print("Loading action {} for test set".format(act))
-    test_dataset = H36motion(path_to_data='h3.6m/', actions=[act], input_n=input_n, output_n=output_n,
-                              sample_rate=sample_rate, split=1, data_mean=data_mean, data_std=data_std, dct_n=dct_n)
+    if opt.dataset == 'h3.6m':
+        test_dataset = H36motion(path_to_data='h3.6m/', actions=[act], input_n=input_n, output_n=output_n,
+                                 sample_rate=sample_rate, split=1, data_mean=data_mean, data_std=data_std, dct_n=dct_n)
+    elif opt.dataset == 'cmu_mocap':
+        test_dataset = CMU_Motion(path_to_data='cmu_mocap/', actions=[act], input_n=input_n, output_n=output_n,
+                                  split=1, data_mean=data_mean, data_std=data_std, dim_used=dim_used, dct_n=dct_n)
     #print(test_dataset.__len__())
     test_data[act] = DataLoader(
         dataset=test_dataset,
@@ -100,6 +112,7 @@ elif output_n == 10:
     eval_frame = [1, 3, 7, 9]
 
 
+first_instance = True
 for act in actions:
     new_action = True
     for i, (inputs, targets, all_seq) in enumerate(train_data[act]):
@@ -111,9 +124,15 @@ for act in actions:
 
         print("For action {} the train z is: {}".format(act, z.shape))
         for sample in z:
-          sample = sample.reshape(384).cpu().detach().numpy()
+          sample = sample.reshape(n_z).cpu().detach().numpy()
           #print(sample.shape)
           df = pd.DataFrame(np.expand_dims(sample, axis=0))
+          if first_instance:
+            df.to_csv('latents/all_train_z.csv', header=False, index=False)
+            first_instance = False
+          else:
+            with open('latents/all_train_z.csv', 'a') as f:
+                df.to_csv(f, header=False, index=False)
           if new_action:
             df.to_csv('latents/'+str(act)+'_train_z.csv', header=False, index=False)
             new_action = False
@@ -131,7 +150,7 @@ for act in actions:
 
         print("For action {} the test z is: {}".format(act, z.shape))
         for sample in z:
-          sample = sample.reshape(384).cpu().detach().numpy()
+          sample = sample.reshape(n_z).cpu().detach().numpy()
           #print(sample.shape)
           df = pd.DataFrame(np.expand_dims(sample, axis=0))
           if new_action:
