283d39340201e3385e895e8256632a3f7e7206d3
bouracha <bouracha@tcd.ie>
2020-12-01 14:08:43 +0000

    Refactoring: Making folder for all models wherein layers, utils, encoders, decoders and architectures will all have their own files.

---
+++ models/layers.py
@@ -0,0 +1,113 @@
+import torch.nn as nn
+import torch
+
+
+class FullyConnected(nn.Module):
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(FullyConnected, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input):
+        output = torch.matmul(input, self.weight)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
+
+
+class GraphConvolution(nn.Module):
+    """
+    adapted from : https://github.com/tkipf/gcn/blob/92600c39797c2bfb61a508e52b88fb554df30177/gcn/layers.py#L132
+    """
+
+    def __init__(self, in_features, out_features, bias=True, node_n=48, out_node_n=None):
+        super(GraphConvolution, self).__init__()
+        if out_node_n is None:
+            out_node_n = node_n
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        self.att = Parameter(torch.FloatTensor(out_node_n, node_n))
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        self.att.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input):
+        support = torch.matmul(input, self.weight)
+        output = torch.matmul(self.att, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
+
+
+class GC_Block(nn.Module):
+    def __init__(self, in_features, p_dropout, bias=True, node_n=48):
+        """
+        Define a residual block of GCN
+        """
+        super(GC_Block, self).__init__()
+        self.in_features = in_features
+        self.out_features = in_features
+
+        self.gc1 = GraphConvolution(in_features, in_features, node_n=node_n, bias=bias)
+        self.bn1 = nn.BatchNorm1d(node_n * in_features)
+
+        self.gc2 = GraphConvolution(in_features, in_features, node_n=node_n, bias=bias)
+        self.bn2 = nn.BatchNorm1d(node_n * in_features)
+
+        self.do = nn.Dropout(p_dropout)
+        self.act_f = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        y = self.gc1(x)
+        b, n, f = y.shape
+        y = self.bn1(y.view(b, -1)).view(b, n, f)
+        y = self.act_f(y)
+        y = self.do(y)
+
+        y = self.gc2(y)
+        b, n, f = y.shape
+        y = self.bn2(y.view(b, -1)).view(b, n, f)
+        y = self.act_f(y)
+        y = self.do(y)
+
+        return y + x
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
---
+++ models/utils.py
@@ -0,0 +1,15 @@
+class AccumLoss(object):
+    def __init__(self):
+        self.reset()
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
