7b2a7e4d0adb80afb99cb761bba285e2c6c4bb4b
bouracha <bouracha@tcd.ie>
2021-01-10 11:28:52 +0000

    ENABLED GPU OR CPU TRAINING

--- main.py
+++ main.py
@@ -39,13 +39,19 @@ opt = parser.parse_args()
 
 
 is_cuda = torch.cuda.is_available()
+if is_cuda:
+    device = "cuda"
+    job = 10 #Can change this to multi-process dataloading
+else:
+    device = "cpu"
+    job = 0
 
 #####################################################
 # Load data
-#####################################################
+####whvih#################################################
 data = DATA("h3.6m", "h3.6m/dataset/")
 out_of_distribution = data.get_poses(input_n=1, output_n=1, sample_rate=2, dct_n=2, out_of_distribution_action=None)
-train_loader, val_loader, OoD_val_loader, test_loader = data.get_dataloaders(train_batch=16, test_batch=128, job=10)
+train_loader, val_loader, OoD_val_loader, test_loader = data.get_dataloaders(train_batch=16, test_batch=128, job=job)
 print(">>> data loaded !")
 print(">>> train data {}".format(data.train_dataset.__len__()))
 print(">>> validation data {}".format(data.val_dataset.__len__()))
@@ -53,30 +59,31 @@ print(">>> validation data {}".format(data.val_dataset.__len__()))
 ##################################################################
 # Instantiate model, and methods used fro training and valdation
 ##################################################################
-n_zs = [2, 3, 5, 10, 20, 48]
-for n_z in n_zs:
-    print(">>> creating model")
-    model = nnmodel.VAE(encoder_layers=[48, 100, 50, n_z],  decoder_layers = [n_z, 50, 100, 48], variational=opt.variational, output_variance=opt.output_variance, device="cuda", batch_norm=opt.batch_norm, p_dropout=0.0, beta=opt.beta)
-    clipping_value = 1
-    torch.nn.utils.clip_grad_norm(model.parameters(), clipping_value)
-    if is_cuda:
-        model.cuda()
-    print(model)
+#n_zs = [2, 3, 5, 10, 20, 48]
+n_z = 48
+#for n_z in n_zs:
+print(">>> creating model")
+model = nnmodel.VAE(encoder_layers=[48, 100, 50, n_z],  decoder_layers = [n_z, 50, 100, 48], variational=opt.variational, output_variance=opt.output_variance, device=device, batch_norm=opt.batch_norm, p_dropout=0.0, beta=opt.beta)
+clipping_value = 1
+torch.nn.utils.clip_grad_norm_(model.parameters(), clipping_value)
+if is_cuda:
+    model.cuda()
+print(model)
 
-    print(">>> total params: {:.2f}M".format(sum(p.numel() for p in model.parameters()) / 1000000.0))
-    lr=0.00001
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+print(">>> total params: {:.2f}M".format(sum(p.numel() for p in model.parameters()) / 1000000.0))
+lr=0.00001
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
 
-    for epoch in range(0, 50):
-        print("Epoch: ", epoch+1)
+for epoch in range(0, 50):
+    print("Epoch: ", epoch+1)
 
-        model.train_epoch(epoch, lr, train_loader, optimizer)
+    model.train_epoch(epoch, lr, train_loader, optimizer)
 
-        model.eval_full_batch(train_loader, 'train')
-        model.eval_full_batch(val_loader, 'val')
+    model.eval_full_batch(train_loader, 'train')
+    model.eval_full_batch(val_loader, 'val')
 
-        model.save_checkpoint_and_csv(epoch, lr, optimizer)
+    model.save_checkpoint_and_csv(epoch, lr, optimizer)
 
 
 
