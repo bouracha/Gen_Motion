e8c0d7fff29f9a2f785b7e47372daca53b954204
bouracha <bouracha@tcd.ie>
2021-01-25 15:42:42 +0000

    Small change to plotting function

--- plot_training_curves.py
+++ plot_training_curves.py
@@ -6,14 +6,21 @@ import numpy as np
 name = sys.argv[0]
 
 def plot_losses(path_to_file, label):
-  data = pd.read_csv(path_to_file)
+  data = pd.read_csv("plot_data/"+str(path_to_file)+".csv")
 
   epoch = np.array(data['Epoch'])
-  reconstruction = np.array(data['val_reconstruction'])
-  #KL = np.array(data['val_KL'])
+  #reconstruction = np.array(data['val_reconstruction'])
+  KL = np.array(data['val_KL'])
 
-  plt.plot(epoch, reconstruction, label=label)
-  #plt.plot(epoch, KL, label=label+"_KL")
+  thermalised_history = 10
+  #print("{} thermalised mean: {}".format(label, np.mean(reconstruction[-thermalised_history:])))
+  #print("{} thermalised STD: {}".format(label, np.std(reconstruction[-thermalised_history:])))
+
+  if label[:2] == "ae":
+    plt.plot(epoch, reconstruction, label=label, linestyle='--')
+  else:
+    #plt.plot(epoch, reconstruction, label=label)
+    plt.plot(epoch, KL, label=label+"_KL")
 
 print("Number of datasets: ", len(sys.argv)-1)
 for i in range(1, len(sys.argv)):
