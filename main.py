bdac6f8ce64ef0c6aa5a8250485517fe96033a85
bouracha <bouracha@tcd.ie>
2021-01-28 16:53:56 +0000

    Updated commandline arugments, n_z, epochs, number of epochs per save, and hidden layers for the encoder and assumes encoder and decoder are symmetrical.

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
 from __future__ import print_function, absolute_import, division
 
-import os
-import time
-import torch
-import torch.nn as nn
 import torch.optim
-from torch.utils.data import DataLoader
-from torch.autograd import Variable
-import numpy as np
-from progress.bar import Bar
-import pandas as pd
-
-from utils import utils as utils
-from utils.opt import Options
-from utils.h36motion import H36motion
-from utils.cmu_motion import CMU_Motion
-from utils.cmu_motion_3d import CMU_Motion3D
-
-import utils.data_utils as data_utils
 
 from data import DATA
 
@@ -35,6 +18,10 @@ parser.add_argument('--use_MNIST', dest='use_MNIST', action='store_true', help='
 parser.add_argument('--use_bernoulli_loss', dest='use_bernoulli_loss', action='store_true', help='toggle to bernoulli of gauss loss')
 parser.add_argument('--beta', type=float, default=1.0, help='Downweighting of the KL divergence')
 parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+parser.add_argument('--n_z', type=int, default=2, help='Number of latent variables')
+parser.add_argument('--n_epochs', type=int, default=50, help='Number of epochs to train for')
+parser.add_argument('--n_epochs_per_save', type=int, default=10, help='Number of epochs before saving the checkpoints')
+parser.add_argument('--encoder_hidden_layers', nargs='+', type=str, default=[500, 200, 100, 50], help='input the out of distribution action')
 parser.set_defaults(variational=False)
 parser.set_defaults(batch_norm=False)
 parser.set_defaults(weight_decay=False)
@@ -48,7 +35,7 @@ weight_decay = 0.0
 if opt.weight_decay:
     weight_decay = 1e-4
 
-lr=opt.lr
+
 
 folder_name=""
 is_cuda = torch.cuda.is_available()
@@ -95,35 +82,48 @@ print(">>> data loaded !")
 ##################################################################
 # Instantiate model, and methods used fro training and valdation
 ##################################################################
-n_zs = [2, 3, 5, 10, 20, 48]
 input_n = data.node_n
-#n_z = 2
+n_z = opt.n_z
+n_epochs = opt.n_epochs
+lr=opt.lr
+encoder_hidden_layers = opt.encoder_hidden_layers
 
-for n_z in n_zs:
-    print(">>> creating model")
-    model = nnmodel.VAE(encoder_layers=[input_n, 500, 200, 100, 50, n_z],  decoder_layers = [n_z, 50, 100, 200, 500, input_n], lr=lr, train_batch_size=train_batch_size, variational=opt.variational, output_variance=opt.output_variance, device=device, batch_norm=opt.batch_norm, weight_decay=weight_decay, p_dropout=0.0, beta=opt.beta, new_model=True, folder_name=folder_name)
-    clipping_value = 1
-    torch.nn.utils.clip_grad_norm_(model.parameters(), clipping_value)
-    if is_cuda:
-        model.cuda()
-    print(model)
+encoder_layers = []
+decoder_layers = []
+encoder_layers.append(input_n)
+decoder_layers.append(n_z)
+n_hidden = len(encoder_hidden_layers)
+for i in range(n_hidden):
+    encoder_layers.append(encoder_hidden_layers[i])
+    decoder_layers.append(n_hidden-1-i)
+encoder_layers.append(n_z)
+decoder_layers.append(input_n)
 
-    print(">>> total params: {:.2f}M".format(sum(p.numel() for p in model.parameters()) / 1000000.0))
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+print(">>> creating model")
+model = nnmodel.VAE(encoder_layers=encoder_layers,  decoder_layers=decoder_layers, lr=lr, train_batch_size=train_batch_size, variational=opt.variational, output_variance=opt.output_variance, device=device, batch_norm=opt.batch_norm, weight_decay=weight_decay, p_dropout=0.0, beta=opt.beta, new_model=True, folder_name=folder_name)
+clipping_value = 1
+torch.nn.utils.clip_grad_norm_(model.parameters(), clipping_value)
+if is_cuda:
+    model.cuda()
+print(model)
+
+print(">>> total params: {:.2f}M".format(sum(p.numel() for p in model.parameters()) / 1000000.0))
+optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
 
-    for epoch in range(0, 50):
-        print("Epoch: ", epoch+1)
+for epoch in range(1, n_epochs+1):
+    print("Epoch: ", epoch)
 
-        if opt.use_MNIST:
-            model.train_epoch_mnist(epoch, train_loader, optimizer, opt.use_bernoulli_loss)
-            model.eval_full_batch_mnist(train_loader, epoch, 'train', opt.use_bernoulli_loss)
-            model.eval_full_batch_mnist(val_loader, epoch, 'val', opt.use_bernoulli_loss)
-        else:
-            model.train_epoch(epoch, train_loader, optimizer)
-            model.eval_full_batch(train_loader, epoch, 'train')
-            model.eval_full_batch(val_loader, epoch, 'val')
+    if opt.use_MNIST:
+        model.train_epoch_mnist(epoch, train_loader, optimizer, opt.use_bernoulli_loss)
+        model.eval_full_batch_mnist(train_loader, epoch, 'train', opt.use_bernoulli_loss)
+        model.eval_full_batch_mnist(val_loader, epoch, 'val', opt.use_bernoulli_loss)
+    else:
+        model.train_epoch(epoch, train_loader, optimizer)
+        model.eval_full_batch(train_loader, epoch, 'train')
+        model.eval_full_batch(val_loader, epoch, 'val')
 
+    if epoch%10 == 0:
         model.save_checkpoint_and_csv(epoch, lr, optimizer)
 
 
