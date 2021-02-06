770c761b64f7907be7a8b404ddc4b340d00a0e8e
bouracha <bouracha@tcd.ie>
2021-02-06 16:27:11 +0000

    Added file to generate samples from VAE of given name architecture etc. The name should link to the folder and everything should be set up with the standard bookeeping.

---
+++ generate_experiments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#python3 generate.py --batch_norm --encoder_hidden_layers 500 400 300 200 100 50 --variational --beta 1.0 --start_epoch 11 --output_variance --name "deep_2_model-var" --grid_size 20
+
+#python3 generate.py --batch_norm --encoder_hidden_layers 500 400 300 200 100 50 --variational --start_epoch 151 --name "deep_2_beta0.01" --grid_size 20
+#python3 generate.py --batch_norm --encoder_hidden_layers 500 400 300 200 100 50 --variational --start_epoch 171 --name "deep_2_beta0.01" --grid_size 20
+#python3 generate.py --batch_norm --encoder_hidden_layers 500 400 300 200 100 50 --variational --start_epoch 11 --name "deep_2_beta0.01" --grid_size 20
+
+#python3 generate.py --batch_norm --encoder_hidden_layers 100 --variational --start_epoch 50 --name "2" --grid_size 20
+
