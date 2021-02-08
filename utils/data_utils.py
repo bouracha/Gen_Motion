fe5f4f865f670307f64d4dba79cae471c9f45972
bouracha <bouracha@tcd.ie>
2021-02-08 09:32:57 +0000

    Define actions: remove phoning and discussions as discovered upside-down poses. (Hacky fix to exclude them but already have enough expression in remaining poses).

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -289,8 +289,13 @@ def define_actions(action, dataset='h3.6m', out_of_distribution=False):
     """
 
     if dataset=='h3.6m' or dataset=='h3.6m_3d':
-        actions = ["walking", "eating", "smoking", "discussion", "directions",
-                  "greeting", "phoning", "posing", "purchases", "sitting",
+        #actions = ["walking", "eating", "smoking", "discussion", "directions",
+        #          "greeting", "phoning", "posing", "purchases", "sitting",
+        #          "sittingdown", "takingphoto", "waiting", "walkingdog",
+        #          "walkingtogether"]
+        #Define actions without phoning and discussions as have upside-down data.
+        actions = ["walking", "eating", "smoking", "directions",
+                  "greeting", "posing", "purchases", "sitting",
                   "sittingdown", "takingphoto", "waiting", "walkingdog",
                   "walkingtogether"]
     elif dataset=='cmu_mocap' or dataset=='cmu_mocap_3d':
